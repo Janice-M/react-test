cc3558abcf56712961a2be2f6df46946e595a118
Janice Muia <janiceink001@gmail.com>
2020-03-18 20:28:56 +0300

    return for empty task list

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -31,9 +31,18 @@ function TaskList({ loading, tasks, onPinTask, onArchiveTask }) {
           );
         }
 
-    if (tasks.length === 0) {
-    return <div className="list-items">empty</div>;
-    }
+        if (tasks.length === 0) {
+          return (
+            <div className="list-items">
+              <div className="wrapper-message">
+                <span className="icon-check" />
+                <div className="title-message">You have no tasks</div>
+                <div className="subtitle-message">Sit back and relax</div>
+              </div>
+            </div>
+          );
+        }
+      
   
     return (
       <div className="list-items">
